691977283769ee1f041e7a3780071c2a8a75d629
Jayvee <jayvee.github0703@gmail.com>
2026-04-30 12:31:29 +0800

    Removed supabase client

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,6 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
 import Shop from './features/shop/Shop'
-import Login from "./features/auth/Login"
-import AuthAdmin from "./features/auth/AuthAdmin"
-import Dashboard from "./features/auth/Dashboard/Dashboard"
-
 
 const router = createBrowserRouter([
   {
@@ -18,20 +14,6 @@ const router = createBrowserRouter([
     path: "/shop",
     element: <Shop />
   },
-  {
-    path: "/dashboard",
-    element: <AuthAdmin />,
-    children: [
-      {
-        index:true,
-        element:<Dashboard/>
-      }
-    ]
-  },
-  {
-    path: "/kate.kors/login",
-    element: <Login />
-  }
 ])
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
